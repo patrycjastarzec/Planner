0e609fc5ad41abec4389caf504547db49ed285b6
patrycjastarzec <starzec.pa@gmail.com>
2021-03-21 21:22:35 +0100

    App refactoring - add to list function and add text input function

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,24 @@
+const goalInput = document.getElementById("todaygoal");
+const mealInput = document.getElementById("todaymeal");
+const toDoInput = document.getElementById("todo");
+const appointmentInput = document.getElementById("appointments");
+const priorityInput = document.getElementById("priority");
+const gratefulInput = document.getElementById("grateful");
+const notesInput = document.getElementById("notes");
+
+const addGoalButton = document.getElementById("addGoal");
+const addMealButton = document.getElementById("addMealButton");
+const addToDoButton = document.getElementById("addToDoButton");
+const addAppointmentButton = document.getElementById("addAppointmentButton");
+const addPriorButton = document.getElementById("addPriorityButton");
+const addGratefulButton = document.getElementById("addGratefulButton");
+const addNotesButton = document.getElementById("addNotesButton");
+
+let goalListArea = document.getElementById("goalsList");
+let mealListArea = document.getElementById("mealsList");
+let tasksListArea = document.getElementById("toDoList");
+let appointmentsListArea = document.getElementById("appointmentsList");
+
+let priorityElement = document.getElementById("priorityElement");
+let gratefulElement = document.getElementById("gratefulElement");
+let notesElement = document.getElementById("notesElement");
