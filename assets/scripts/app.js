05fd3d622e8aabdcae0b536ab891d18d645be64d
patrycjastarzec <patryc.sc@gmail.com>
2022-08-02 18:11:04 +0200

    Added main page template

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,14 +4,13 @@ let myMealList = [];
 let myToDoList = [];
 let myAppointmentsList = [];
 
-function addListElement(input, myList, listArea, limit) {
+function addListElement(input, list, listArea, limit) {
   let userInput = input.value;
-  if (userInput.length > 0 && myList.length < limit) {
-      myList.push(userInput);
-      let newElement = document.createElement("li");
-      newElement.innerHTML = userInput;
-      listArea.appendChild(newElement);
-      console.log(myList.length); // to check the length of list in browser developer tools
+  if (userInput.length > 0 && list.length < limit) {
+    list.push(userInput);
+    let newElement = document.createElement("li");
+    newElement.innerHTML = userInput;
+    listArea.appendChild(newElement);
   }
   clearTextArea(input);
 }
@@ -20,10 +19,28 @@ function clearTextArea(areaToClear) {
   areaToClear.value = "";
 }
 
-addGoalButton.addEventListener("click", addListElement.bind(this, goalInput, myGoalList, goalListArea, 5));
-addMealButton.addEventListener("click", addListElement.bind(this, mealInput, myMealList, mealListArea, 5));
-addToDoButton.addEventListener("click", addListElement.bind(this, toDoInput, myToDoList, tasksListArea, 10));
-addAppointmentButton.addEventListener("click", addListElement.bind(this, appointmentInput, myAppointmentsList, appointmentsListArea, 10));
+addGoalButton.addEventListener(
+  "click",
+  addListElement.bind(this, goalInput, myGoalList, goalListArea, 5)
+);
+addMealButton.addEventListener(
+  "click",
+  addListElement.bind(this, mealInput, myMealList, mealListArea, 5)
+);
+addToDoButton.addEventListener(
+  "click",
+  addListElement.bind(this, toDoInput, myToDoList, tasksListArea, 10)
+);
+addAppointmentButton.addEventListener(
+  "click",
+  addListElement.bind(
+    this,
+    appointmentInput,
+    myAppointmentsList,
+    appointmentsListArea,
+    10
+  )
+);
 
 //-----------------Text input-----------------
 
@@ -31,46 +48,75 @@ function addTextElement(input, myElement) {
   let userInput = input.value;
   let newElement = document.createElement("p");
   newElement.innerHTML = userInput;
-  if(myElement.children.length < 1){
+  if (myElement.children.length < 1) {
     myElement.appendChild(newElement);
   }
   clearTextArea(input);
 }
 
-addPriorButton.addEventListener("click", addTextElement.bind(this, priorityInput, priorityElement));
-addGratefulButton.addEventListener("click", addTextElement.bind(this, gratefulInput, gratefulElement));
-addNotesButton.addEventListener("click", addTextElement.bind(this, notesInput, notesElement));
+addPriorButton.addEventListener(
+  "click",
+  addTextElement.bind(this, priorityInput, priorityElement)
+);
+addGratefulButton.addEventListener(
+  "click",
+  addTextElement.bind(this, gratefulInput, gratefulElement)
+);
+addNotesButton.addEventListener(
+  "click",
+  addTextElement.bind(this, notesInput, notesElement)
+);
 
 //-----------------Remove text input------------
 
-function removeTextElement(myElement){
+function removeTextElement(myElement) {
   myElement.innerHTML = "";
 }
 
-removePriorButton.addEventListener('click', removeTextElement.bind(this, priorityElement));
-removeGratefulButton.addEventListener('click', removeTextElement.bind(this, gratefulElement));
-removeNotesButton.addEventListener('click', removeTextElement.bind(this, notesElement));
+removePriorButton.addEventListener(
+  "click",
+  removeTextElement.bind(this, priorityElement)
+);
+removeGratefulButton.addEventListener(
+  "click",
+  removeTextElement.bind(this, gratefulElement)
+);
+removeNotesButton.addEventListener(
+  "click",
+  removeTextElement.bind(this, notesElement)
+);
 
 //-----------------Remove list input------------
 
-function removeListElement(listArea, myList){
-  if(myList.length > 0){
+function removeListElement(listArea, myList) {
+  if (myList.length > 0) {
     listArea.removeChild(listArea.lastElementChild);
     myList.pop();
     console.log(myList.length); // to check the length of list in browser developer tools
   }
 }
 
-removeGoalButton.addEventListener('click', removeListElement.bind(this, goalListArea, myGoalList));
-removeMealButton.addEventListener('click', removeListElement.bind(this, mealListArea, myMealList));
-removeToDoButton.addEventListener('click', removeListElement.bind(this, tasksListArea, myToDoList));
-removeAppointmentButton.addEventListener('click', removeListElement.bind(this, appointmentsListArea, myAppointmentsList));
-
+removeGoalButton.addEventListener(
+  "click",
+  removeListElement.bind(this, goalListArea, myGoalList)
+);
+removeMealButton.addEventListener(
+  "click",
+  removeListElement.bind(this, mealListArea, myMealList)
+);
+removeToDoButton.addEventListener(
+  "click",
+  removeListElement.bind(this, tasksListArea, myToDoList)
+);
+removeAppointmentButton.addEventListener(
+  "click",
+  removeListElement.bind(this, appointmentsListArea, myAppointmentsList)
+);
 
 //-----------------Printing-----------------
 
 function printView() {
-	const pageWithPlan = document.getElementById("pageWithPlan");
+  const pageWithPlan = document.getElementById("pageWithPlan");
   const formArea = document.getElementById("form-area");
   formArea.style.display = "none";
   window.print();
@@ -78,10 +124,9 @@ function printView() {
 
 printButton.addEventListener("click", printView);
 
-
 //-----------------Clearing-----------------
 
-function clearPlan(){
+function clearPlan() {
   goalListArea.innerHTML = "";
   mealListArea.innerHTML = "";
   tasksListArea.innerHTML = "";
@@ -92,3 +137,4 @@ function clearPlan(){
 }
 
 clearButton.addEventListener("click", clearPlan);
+
