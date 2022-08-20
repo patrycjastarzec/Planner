c1ff2524c88eeb1d83cd432d86a633c94c8a2ee4
patrycjastarzec <patryc.sc@gmail.com>
2022-08-20 22:13:35 +0200

    Changes adding and removing buttons classes names in plan subpage

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,28 +1,48 @@
-const goalInput = document.getElementById("todaygoal");
-const mealInput = document.getElementById("todaymeal");
-const toDoInput = document.getElementById("todo");
-const appointmentInput = document.getElementById("appointments");
-const priorityInput = document.getElementById("priority");
-const gratefulInput = document.getElementById("grateful");
-const notesInput = document.getElementById("notes");
-
-const addGoalButton = document.getElementById("addGoalButton");
-const addMealButton = document.getElementById("addMealButton");
-const addToDoButton = document.getElementById("addToDoButton");
-const addAppointmentButton = document.getElementById("addAppointmentButton");
-const addPriorButton = document.getElementById("addPriorityButton");
-const addGratefulButton = document.getElementById("addGratefulButton");
-const addNotesButton = document.getElementById("addNotesButton");
-
-const removeGoalButton = document.getElementById("removeGoalButton");
-const removeMealButton = document.getElementById("removeMealButton");
-const removeToDoButton = document.getElementById("removeToDoButton");
-const removeAppointmentButton = document.getElementById(
-  "removeAppointmentButton"
+const goalInput = document.getElementById("form-area__input-goal");
+const mealInput = document.getElementById("form-area__input-todaymeal");
+const toDoInput = document.getElementById("form-area__input-todo");
+const appointmentInput = document.getElementById(
+  "form-area__input-appointments"
 );
-const removePriorButton = document.getElementById("removePriorButton");
-const removeGratefulButton = document.getElementById("removeGratefulButton");
-const removeNotesButton = document.getElementById("removeNotesButton");
+const priorityInput = document.getElementById("form-area__input-priority");
+const gratefulInput = document.getElementById("form-area__input-grateful");
+const notesInput = document.getElementById("form-area__input-notes");
+
+const addGoalButton = document.getElementById("form-area__add-goal-button");
+const addMealButton = document.getElementById("form-area__add-meal-button");
+const addToDoButton = document.getElementById("form-area__add-todo-button");
+const addAppointmentButton = document.getElementById(
+  "form-area__add-appointment-button"
+);
+const addPriorButton = document.getElementById(
+  "form-area__add-priority-button"
+);
+const addGratefulButton = document.getElementById(
+  "form-area__add-grateful-button"
+);
+const addNotesButton = document.getElementById("form-area__add-notes-button");
+
+const removeGoalButton = document.getElementById(
+  "form-area__remove-goal-button"
+);
+const removeMealButton = document.getElementById(
+  "form-area__remove-meal-button"
+);
+const removeToDoButton = document.getElementById(
+  "form-area__remove-todo-button"
+);
+const removeAppointmentButton = document.getElementById(
+  "form-area__remove-appointment-button"
+);
+const removePriorButton = document.getElementById(
+  "form-area__remove-priority-button"
+);
+const removeGratefulButton = document.getElementById(
+  "form-area__remove-grateful-button"
+);
+const removeNotesButton = document.getElementById(
+  "form-area__remove-notes-button"
+);
 
 const printButton = document.getElementById("form-area__print-button");
 const clearButton = document.getElementById("form-area__clear-button");
